c2ab66d53bd71aa3884edfbe7d85da433164e7cc
Nilay Verma <nilayverma10@gmail.com>
2024-04-13 02:15:18 +0530

    yo

--- index.js
+++ index.js
@@ -8,10 +8,6 @@ app.set("views", path.join(__dirname, "/views"))
 app.listen(5000, () => {
     console.log("Listening on port 5000")
 })
-
-app.get("/", (req, res) => {
-    res.render("home")
-})
 app.use((req, res) => {
     console.log("Got a request!")
     res.send("This is a response")
